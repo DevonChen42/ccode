deb1260abf92e0f4f34c5e6af9436e54633d717b
DevonChen42 <31545136+DevonChen42@users.noreply.github.com>
2018-01-12 09:06:10 -0800

    Add files via upload

--- hellotime.c
+++ hellotime.c
@@ -1,11 +1,11 @@
-// ******* hellowtime.c *******
+// ******* hellotime.c *******
 #include <stdio.h>
 #include <time.h>
 int main(){
-	time t_t = tine(NULL);
+	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
 	
 	printf("now: %d-%d-%d %d:%d:%d\n",
-	tm.tm year + 1900, tm.tm mon + 1,
-	tm.tm mday, tm.tm hour, tm.tm min, tm.tm sec);
+	tm.tm_year + 1900, tm.tm_mon + 1,
+	tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
--- squarefunction.c
+++ squarefunction.c
@@ -6,16 +6,17 @@ int square ( int v );
 int main( )
 {
 	int x, n ;
-	printf ( "\mEnter some number for finding square \n")
+	printf ( "\nEnter some number for finding square \n");
 	scanf ( "%d", &n) ;
 	x = square ( n ) ;
 	printf ( "\nSquare of the given number %d is %d",n,x );
+	printf("\n\n");
 	return 0;
 }
 
-int square ( int v )	\\ function definition
+int square ( int v )	// function definition
 {
 	int p ;
 	p = v * v ;
-	return (p ) ;
+	return ( p ) ;
 }
